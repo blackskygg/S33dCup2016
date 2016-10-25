86d763246a894a2c6828779658bcfaae16a77904
hustanhuang <andrewhuang@hust.edu.cn>
2016-10-25 11:53:53 +0800

    keywords

--- python_snippet/tokenize.py
+++ python_snippet/tokenize.py
@@ -2,42 +2,54 @@
 
 import re
 
+keywords = [
+    "int",
+    "printf",
+    "if",
+    "else",
+    "for",
+    "while",
+    "do"
+]
+
 type_name = [
-    "SPACE",
-    "CRLF",
-    "SYMBOL",
-    "INTEGER",
-    "STRING",
-    "COMMENT",
-    "SEMICOLON",
-    "COMMA",
-    "ASSIGN",
-    "LPARENTHESE",
-    "RPARENTHESE",
-    "LBRACE",
-    "RBRACE",
-    "LT",
-    "GT",
-    "EQ",
-    "NE",
-    "LE",
-    "GE",
-    "ADD",
-    "SUB",
-    "MUL",
-    "DIV",
-    "INC",
-    "DEC"
+    " ",
+    "\n",
+    "SYM",
+    "INT",
+    "STR",
+    "COM1",
+    "COM2",
+    ";",
+    ",",
+    "=",
+    "(",
+    ")",
+    "{",
+    "}",
+    "<",
+    ">",
+    "==",
+    "!=",
+    "<=",
+    ">=",
+    "+",
+    "-",
+    "*",
+    "/",
+    "++",
+    "--"
 ]
 
 patterns = [
     re.compile(pattern) for pattern in
     [r'(?: |\t)+',
-        r'\n',
+        r'\n',  # python automatically translate \r\n into \n
         r'[_a-zA-Z][_a-zA-Z0-9]*',
         r'(?:0|[1-9][0-9]*)',
         r'"(?:\\.|[^"])*"',
-        r'(?:\/\*.*?\*\/)|(?:\/\/.*)',
+        r'\/\*(?:.|\n)*?\*\/',  # CRLF -> LF, same as above
+        r'\/\/.*\n',  # CRLF -> LF, same as above
         r';',
         r',',
         r'=',
@@ -65,9 +77,33 @@ raw = fin.read()
 
 tokens = []
 while raw:
-    candidates = map(lambda m: m.end() if m else 0,  [pattern.match(raw) for pattern in patterns])
-    candidate_len = max(candidates)
-    tokens.append(raw[:candidate_len])
-    raw = raw[candidate_len:]
+    candidates = [
+        m.end() if m else 0
+        for m in [pattern.match(raw) for pattern in patterns]
+    ]
 
-print("".join(tokens))
+    type_i = 0
+    token_len = 0
+    for i in range(len(candidates)):
+        if candidates[i] > token_len:
+            token_len = candidates[i]
+            type_i = i
+    token, raw = raw[:token_len], raw[token_len:]
+
+    if type_name[type_i] == "COM1":
+        for i in range(token.count('\n')):
+            tokens.append("NL")
+    elif type_name[type_i] == "COM2":
+        tokens.append("NL")
+    elif type_name[type_i] == "SYM":
+        if token in keywords:
+            tokens.append(token)
+        else:
+            tokens.append((type_name[type_i], token))
+    elif type_name[type_i] == "INT":
+        tokens.append((type_name[type_i], token))
+    else:
+        tokens.append(type_name[type_i])
+
+for t in tokens:
+    print(repr(t))
