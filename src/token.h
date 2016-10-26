2bdbf593a5e10d8b6c0d37051a38630d27a41e77
hustanhuang <andrewhuang@hust.edu.cn>
2016-10-26 18:39:20 +0800

    no message

--- src/token.h
+++ src/token.h
@@ -4,15 +4,40 @@
 #include <regex.h>
 
 enum token_type {
-    INT_CONST,
-    ID,
-    STRING,
-    INT,
-    SEMI_COLON,
+    INT_CONST,  // 123
+    STRING,     // "hello"
+    INT,        // int
+    IF,         // if
+    ELSE,       // else
+    WHILE,      // while
+    DO,         // do
+    FOR,        // for
+    BREAK,      // break
+    ID,         // variable_a
+    COMMA,      // ,
+    SEMI_COLON, // ;
+    LBRACE,     // {
+    RBRACE,     // }
+    LPAREN,     // (
+    RPAREN,     // )
+    EQ,         // ==
+    NE,         // !=
+    LT,         // <
+    GT,         // >
+    LE,         // <=
+    GE,         // >=
+    ASSIGN,     // = | += | -= | *= | /=
+    INC,        // ++
+    DEC,        // --
+    ADD,        // +
+    SUB,        // -
+    MUL,        // *
+    DIV,        // /
 };
 
-struct token_regex {
-    regex_t re;
+struct token {
+    enum token_type type;
+    char *content;
 };
 
 #endif
