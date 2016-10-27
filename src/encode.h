660e5e1aa7ac40714ecb9158955d7e1ce2ddc965
hustanhuang <andrewhuang@hust.edu.cn>
2016-10-27 11:07:11 +0800

    abandon encoding design

--- src/encode.h
+++
@@ -1,8 +0,0 @@
-#ifndef ENCODE_H
-#define ENCODE_H
-
-#include "token.h"
-
-char *token_type_string(struct token tokens[], size_t token_count);
-
-#endif
--- src/main.c
+++ src/main.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "token.h"
-#include "encode.h"
 
 struct token tokens[65536];
 
@@ -19,6 +18,8 @@ char *read_code(const char *fname)
 
     return buf;
 }
+
+void print_type_string(struct token tokens[], size_t token_count);
 
 int main(int argc, char const *argv[])
 {
@@ -50,17 +51,54 @@ int main(int argc, char const *argv[])
     }
     printf("token_count = %ld\n", token_count);
 
-    // MALLOC type_string
-    char *type_string = token_type_string(tokens, token_count);
-
     // DEBUG display
-    printf("%s\n", type_string);
-
-    // FREE type_string
-    free(type_string);
+    print_type_string(tokens, token_count);
 
     // FREE code
     free(code);
 
     return 0;
+}
+
+void print_type_string(struct token tokens[], size_t token_count)
+{
+    for (size_t i = 0; i < token_count; i++) {
+        char c;
+        switch (tokens[i].type) {
+        case INT_CONST: c = 'N'; break;
+        case STRING:    c = 'S'; break;
+        case INT_DECL:  c = 'd'; break;
+        case IF:        c = 'i'; break;
+        case ELSE:      c = 'e'; break;
+        case WHILE:     c = 'w'; break;
+        case DO:        c = 'd'; break;
+        case FOR:       c = 'f'; break;
+        case BREAK:     c = 'b'; break;
+        case PRINTF:    c = 'p'; break;
+        case COMMA:     c = ','; break;
+        case SEMI_COLON: c = ';'; break;
+        case LBRACE:    c = '{'; break;
+        case RBRACE:    c = '}'; break;
+        case LPAREN:    c = '('; break;
+        case RPAREN:    c = ')'; break;
+        case EQ:        c = '='; break;
+        case NE:        c = '~'; break;
+        case LE:        c = 'l'; break;
+        case GE:        c = 'g'; break;
+        case LT:        c = '<'; break;
+        case GT:        c = '>'; break;
+        case ASSIGN:    c = ':'; break;
+        case INC:       c = '^'; break;
+        case DEC:       c = 'v'; break;
+        case ADD:       c = '+'; break;
+        case SUB:       c = '-'; break;
+        case MUL:       c = '*'; break;
+        case DIV:       c = '/'; break;
+        case ID:        c = 'I'; break;
+        default:        c = '\0'; break;
+        }
+        putchar(c);
+    }
+    putchar('\n');
+    return;
 }
