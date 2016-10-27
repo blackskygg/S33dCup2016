4b2fbf665ead0eb07b85a11e5758df45ba0c7229
Andrew Huang <andrewhuang@hust.edu.cn>
2016-10-27 19:47:50 +0800

    remove type_string

--- src/main.c
+++ src/main.c
@@ -19,8 +19,6 @@ char *read_code(const char *fname)
     return buf;
 }
 
-void print_type_string(struct token tokens[], size_t token_count);
-
 int main(int argc, char const *argv[])
 {
     token_regex_init();
@@ -51,54 +49,8 @@ int main(int argc, char const *argv[])
     }
     printf("token_count = %ld\n", token_count);
 
-    // DEBUG display
-    print_type_string(tokens, token_count);
-
     // FREE code
     free(code);
 
     return 0;
-}
-
-void print_type_string(struct token tokens[], size_t token_count)
-{
-    for (size_t i = 0; i < token_count; i++) {
-        char c;
-        switch (tokens[i].type) {
-        case INT_CONST: c = 'N'; break;
-        case STRING:    c = 'S'; break;
-        case DECL:      c = 'd'; break;
-        case IF:        c = 'i'; break;
-        case ELSE:      c = 'e'; break;
-        case WHILE:     c = 'w'; break;
-        case DO:        c = 'd'; break;
-        case FOR:       c = 'f'; break;
-        case BREAK:     c = 'b'; break;
-        case PRINTF:    c = 'p'; break;
-        case COMMA:     c = ','; break;
-        case SEMI_COLON: c = ';'; break;
-        case LBRACE:    c = '{'; break;
-        case RBRACE:    c = '}'; break;
-        case LPAREN:    c = '('; break;
-        case RPAREN:    c = ')'; break;
-        case EQ:        c = '='; break;
-        case NE:        c = '~'; break;
-        case LE:        c = 'l'; break;
-        case GE:        c = 'g'; break;
-        case LT:        c = '<'; break;
-        case GT:        c = '>'; break;
-        case ASSIGN:    c = ':'; break;
-        case INC:       c = '^'; break;
-        case DEC:       c = 'v'; break;
-        case ADD:       c = '+'; break;
-        case SUB:       c = '-'; break;
-        case MUL:       c = '*'; break;
-        case DIV:       c = '/'; break;
-        case ID:        c = 'I'; break;
-        default:        c = '\0'; break;
-        }
-        putchar(c);
-    }
-    putchar('\n');
-    return;
 }
