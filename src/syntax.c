450763b30a477179e1655d3eef7ff0c7b6eddc6c
Andrew Huang <andrewhuang@hust.edu.cn>
2016-10-28 11:32:29 +0800

    init_decl

--- src/syntax.c
+++ src/syntax.c
@@ -10,163 +10,177 @@ extern struct token tokens[65536];
 
 struct syntax_node *generate_ast()
 {
-    size_t iter = 0;
-    struct syntax_node *root = stat_list(&iter);
+	size_t iter = 0;
+	struct syntax_node *root = stat_list(&iter);
 
-    return root;
+	return root;
 }
 
 void destory_ast(struct syntax_node *root)
 {
-    if (root) {
-        destory_ast(root->children);
-        destory_ast(root->sibling);
-        free(root);
-    }
+	if (root) {
+		destory_ast(root->children);
+		destory_ast(root->sibling);
+		free(root);
+	}
 }
 
 void print_ast(struct syntax_node *root, size_t level)
 {
-    if (root) {
-        struct token *t = tokens + root->token_idx;
+	if (root) {
+		struct token *t = tokens + root->token_idx;
 
-        for (size_t i = 0; i < level; i++)
-            putchar('\t');
-        printf("(%d) [%d] \"", root->type, t->type);
-        for (size_t i = 0; i < t->length; i++)
-            putchar(t->literal[i]);
-        printf("\"\n");
+		for (size_t i = 0; i < level; i++)
+			putchar('\t');
+		printf("(%d) [%d] \"", root->type, t->type);
+		for (size_t i = 0; i < t->length; i++)
+			putchar(t->literal[i]);
+		printf("\"\n");
 
-        print_ast(root->children, level + 1);
-        print_ast(root->sibling, level);
-    }
+		print_ast(root->children, level + 1);
+		print_ast(root->sibling, level);
+	}
 }
 
 struct syntax_node *stat_list(size_t *idx)
 {
-    struct syntax_node *head = malloc_node();
+	struct syntax_node *head = malloc_node();
 
-    head->type = STAT_LIST;
-    head->token_idx = *idx;
-    head->children = stat(idx);
+	head->type = STAT_LIST;
+	head->token_idx = *idx;
+	head->children = stat(idx);
 
-    struct syntax_node *iter = head->children;
-    while (iter) {
-        iter->sibling = stat(idx);
-        iter = iter->sibling;
-    }
+	struct syntax_node *iter = head->children;
+	while (iter) {
+		iter->sibling = stat(idx);
+		iter = iter->sibling;
+	}
 
-    head->sibling = NULL;
+	head->sibling = NULL;
 
-    return head;
+	return head;
 }
 
 struct syntax_node *stat(size_t *idx)
 {
-    struct syntax_node *root;
+	struct syntax_node *root;
 
-    switch (tokens[*idx].type) {
-    case DECL: root = decl_stat(idx); break;
-    case LBRACE: root = compound_stat(idx); break;
-    case IF: root = selection_stat(idx); break;
-    case WHILE: root = while_stat(idx); break;
-    case DO: root = do_while_stat(idx); break;
-    case FOR: root = for_stat(idx); break;
-    case BREAK: root = jump_stat(idx); break;
-    case PRINTF: root = print_stat(idx); break;
-    default: root = exp_stat(idx); break;
-    }
-    return root;
+	switch (tokens[*idx].type) {
+		case DECL: root = decl_stat(idx); break;
+		case LBRACE: root = compound_stat(idx); break;
+		case IF: root = selection_stat(idx); break;
+		case WHILE: root = while_stat(idx); break;
+		case DO: root = do_while_stat(idx); break;
+		case FOR: root = for_stat(idx); break;
+		case BREAK: root = jump_stat(idx); break;
+		case PRINTF: root = print_stat(idx); break;
+		default: root = exp_stat(idx); break;
+	}
+	return root;
 }
 
 struct syntax_node *decl_stat(size_t *idx)
 {
-    struct syntax_node *node = malloc_node();
+	struct syntax_node *node = malloc_node();
 
-    node->type = SYN_DECL;
-    node->token_idx = *idx;
+	node->type = SYN_DECL;
+	node->token_idx = *idx;
 
-    consume(DECL);
+	consume(DECL);
 
-    node->children = init_decl_list(idx);
+	node->children = init_decl_list(idx);
 
-    consume(SEMI_COLON);
+	consume(SEMI_COLON);
 
-    node->sibling = NULL;
+	node->sibling = NULL;
 
-    return node;
+	return node;
 }
 
 struct syntax_node *init_decl_list(size_t *idx)
 {
-    struct syntax_node *head = malloc_node();
+	struct syntax_node *head = malloc_node();
 
-    head->type = INIT_DECL_LIST;
-    head->token_idx = *idx;
-    head->children = init_decl(idx);
+	head->type = INIT_DECL_LIST;
+	head->token_idx = *idx;
+	head->children = init_decl(idx);
 
-    struct syntax_node *iter = head->children;
-    while (iter) {
-        consume(COMMA);
-        iter->sibling = init_decl(idx);
-        iter = iter->sibling;
-    }
+	struct syntax_node *iter = head->children;
+	while (iter) {
+		consume(COMMA);
+		iter->sibling = init_decl(idx);
+		iter = iter->sibling;
+	}
 
-    head->sibling = NULL;
+	head->sibling = NULL;
 
-    return head;
+	return head;
 }
 
 struct syntax_node *init_decl(size_t *idx)
 {
-    if (!check(ID))
-        return NULL;
-    struct syntax_node *node = malloc_node();
-    node->type = INIT_DECL;
-    node->token_idx = *idx;
-    consume(ID);
-    node->children = NULL;
-    node->sibling = NULL;
-    return node;
+	if (!check(ID))
+		return NULL;
+
+	struct syntax_node *node = malloc_node();
+	node->type = INIT_DECL;
+	node->token_idx = *idx;
+
+	node->children = malloc_node();
+	node->children->type = SYN_ID;
+	node->children->token_idx = *idx;
+
+	consume(ID);
+
+	if (check(ASSIGN)) {
+		consume(ASSIGN);
+		node->children->sibling = expression(idx);
+	} else {
+		node->children->sibling = NULL;
+	}
+
+	node->sibling = NULL;
+
+	return node;
 }
 
 struct syntax_node *exp_stat(size_t *idx)
 {
-    return NULL;
+	return NULL;
 }
 struct syntax_node *compound_stat(size_t *idx)
 {
-    return NULL;
+	return NULL;
 }
 struct syntax_node *selection_stat(size_t *idx)
 {
-    return NULL;
+	return NULL;
 }
 struct syntax_node *while_stat(size_t *idx)
 {
-    return NULL;
+	return NULL;
 }
 struct syntax_node *do_while_stat(size_t *idx)
 {
-    return NULL;
+	return NULL;
 }
 struct syntax_node *for_stat(size_t *idx)
 {
-    return NULL;
+	return NULL;
 }
 struct syntax_node *jump_stat(size_t *idx)
 {
-    return NULL;
+	return NULL;
 }
 struct syntax_node *print_stat(size_t *idx)
 {
-    return NULL;
+	return NULL;
 }
 struct syntax_node *argument_list(size_t *idx)
 {
-    return NULL;
+	return NULL;
 }
 struct syntax_node *expression(size_t *idx)
 {
-    return NULL;
+	return NULL;
 }
